8721ca5cb3b34a9ca163c9f2fdf1415027954567
kyajoo <dovlatyanhayk2@gmail.com>
2023-05-29 20:41:07 +0400

    p

---
+++ script.js
@@ -0,0 +1,52 @@
+// for(let i = 0; i <= 700; i++){                              
+//     if(i >= 100 && i <= 500) {                               
+//         continue;  // continue - sharunakel, menq karox enq break@ ev continue-n ogtagorcel for-i mej                             
+//     }   
+//     else if(i > 600) {
+//         break;
+//     } 
+//     document.writeln(i);                                
+// }        
+
+// let text = 'Ararat';
+
+// text[0] - arajin tari heta ashxsatum u tenc text[1], text[2] ev ayln
+// bayc karoxa menq unenq 1000 hat tar u uzum enq amen meki het ashxatenq dra hamar grum enq orinaki hamar i < text.length u fsyo 1000-i het el kashxati miayn petq e grenq poqr e (<) text.length (text-n el partadir chi orinaka)
+
+// String Methods
+
+// 1. indexOf() - bari mej voreve tari hamar e asum ete grum enq nenc tar vor chka misht -1-a talis arjeq@, ete uzum enq eli tareri hamar asi loop-ov enq anum, ete promptov enq anum u xar@ tarer enq grum karanq nenc anenq vor asi vor tar@ ka texti mej vor@ chka da anum enq loop-ov
+// 2. lastIndexOf() -
+// 3. charAt()
+// 4. charCodeAt()
+// 5. slice()
+// 6. replace()
+// 7. replaceAll()
+// 8. concat()
+// 9. includes()
+// 10. toUpperCase()
+// 11. toLowerCase()
+// length
+
+// let text = 'Learning';
+// alert(text.indexOf('j'));
+
+// let p = prompt('Enter your letters');
+// for(let i = 0; i < p.length; i++){
+//     if(text.indexOf(p[i]) !== -1){
+//         alert(p[i] + ' texti mej arka e')
+//     }
+//     else{
+//         alert(p[i] + ' texti mej arka che');
+//     }
+// }
+
+// document.write(text[0]);
+// inch@ uni qanak karanq ogtagorcenq []- es pakagic@
+// indeqsavorum@ -1-a isk length-n +1-a aysinqn indeqsavorum@ sksuma )-ic isk length-n 1-ica sksum
+// for(let i = 0; i < text.length; i++){
+//     if(text[i] == 'r') {
+//         alert('ayooo');
+//     }
+// }
+
